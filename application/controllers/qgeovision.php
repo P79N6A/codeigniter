4f69f6ff8c74a450317ab1a52f3a8d4cd2864892
Finc8888 <con8@mail.ru>
2018-07-06 23:49:30 +0300

    qgeovision extendet

--- application/controllers/qgeovision.php
+++ application/controllers/qgeovision.php
@@ -5,68 +5,46 @@
 */
 
 class Qgeovision extends CI_Controller {
-
 	function __construct(){
 		parent::__construct();
-//        $this->checkLogin();
 		$this->load->helper('url');
         $this->load->helper('file');
 //		$this->load->model("mdatamodel");
         
 	}
-	
 	public function index()
 	{
         $this->load->view('vqgeovision');
 	}
 function parse_xml(){
-//    $path_to_xml = base_url()."uploads/tiod/peter_region.xml";
-//    echo $path_to_xml;
-//    echo file_exists('./uploads/tiod/peter_region.xml');
-//    $string = read_file('./uploads/tiod/peter_region.xml');
-//    echo $string;
-//    //$str_xml = $this->file->read_file($path_to_xml);
-
-    //$xmlfile = dirname(__dir__).'\..\..\..\uploads\tiod\peter_region.xml';
-    $xmlfile = '.\uploads\tiod\peter_region.xml';
-
-    //$xmlfile = ".";
-
-//    echo $xmlfile;
+//$xmlfile = dirname(__dir__).'\..\..\..\uploads\tiod\peter_region.xml';
+$xmlfile = '.\uploads\tiod\peter_region.xml';
     if (file_exists($xmlfile)) {
         $xml = simplexml_load_file($xmlfile);
-//        echo var_dump($xml);
+//      echo var_dump($xml);
         foreach($xml->Item as $item){
             $names_text_value = $item['Name'];
-//            $coordinates_text_value = $item['Code'];
-//            print "$names_text_value\n";
-//            foreach($item->children as $child)
-                echo var_dump($item->children());break;
-
-//            echo var_dump($item->children());break;
-//            print "$coordinates_text_value\n";
+            echo var_dump($xml);
+//            echo $names_text_value;
+            $child = array();
+            array_push($child,$item);
+        //echo var_dump($item->children());break;
         }
-
-
-
-
-
-
-
-        $json = json_encode($xml);
-//        echo var_dump($json);
-        $array = json_decode($json, TRUE);
-
-        $data['xml'] = $xml;
-        $this->load->vars($data);
-        $this->load->view('vqgeovision');
-
+//        $json = json_encode($xml);
+        //echo var_dump($json);
+//        $array = json_decode($json, TRUE);
+//        $data['xml'] = $xml;
+//        $this->load->vars($data);
+//        $this->load->view('vqgeovision');
     } else {
         exit('Не удалось открыть файл отсутствует.');
     }
+    $obj = $child[0]->Attribute[2]["Value"];
+//    echo(var_dump($obj));
+
+//        echo $obj[0];
 
 }
 }
-
 /* End of file welcome.php */
 /* Location: ./application/controllers/welcome.php */
